f0d742b56bb4d5b0f1b47f0e2c57a12374393c14
Hanzala <90458478+hanzala-nickelfox@users.noreply.github.com>
2024-01-25 15:27:05 +0530

    Revert "Feat/header and sidebar"

--- src/layouts/privateLayoutStyles.js
+++ src/layouts/privateLayoutStyles.js
@@ -1,88 +1,69 @@
-import { useTheme } from "@mui/system";
+import { useTheme } from "@mui/system"
 
-const drawerWidth = 300;
+const drawerWidth = 270
 
 export const useStyles = () => {
-  const theme = useTheme();
+  const theme = useTheme()
 
   return {
     drawer: {
       width: drawerWidth,
       flexShrink: 0,
       "& .MuiDrawer-paper": {
-        background: theme.palette.primary.black,
+        margin: "8px",
+        marginBottom: "66px",
+        background: theme.palette.background.secondary,
+        borderRadius: "5px",
         width: drawerWidth - 16,
         boxSizing: "border-box",
-        height: "100vh",
-      },
+        height: "98vh"
+      }
     },
     drawerHeader: {
-      color: theme.palette.text.white,
+      color: theme.palette.text.main
     },
     divider: {
       border: `1px solid rgba(255, 255, 255, 0.1)`,
-      marginBottom: "21px",
+      marginBottom: "21px"
     },
     activeListItem: {
       color: theme.palette.text.white,
-      backgroundColor: theme.palette.accents.cards,
+      backgroundColor: theme.palette.primary.main,
       width: "90%",
       margin: "auto",
       borderRadius: "5px",
       padding: "16px",
       "&:hover": {
-        backgroundColor: theme.palette.accents.cards,
-      },
+        backgroundColor: theme.palette.secondary.main
+      }
     },
     listItem: {
-      color: theme.palette.text.white,
+      color: theme.palette.text.main,
       width: "90%",
       margin: "auto",
       borderRadius: "5px",
-      padding: "16px",
-      "&:hover": {
-        backgroundColor: theme.palette.accents.cards,
-      },
+      padding: "16px"
     },
     listItemText: {
       fontFamily: theme.typography.fontFamily,
       fontSize: "14px",
       fontWeight: "400",
-      lineHeight: "16px",
-    },
-    listItemTextActive: {
-      fontFamily: theme.typography.fontFamily,
-      fontSize: "14px",
-      fontWeight: "400",
-      lineHeight: "16px",
-      color: theme.palette.primary.main,
+      lineHeight: "16px"
     },
     icon: {
-      color: theme.palette.primary.white,
+      color: theme.palette.text.main
     },
     iconActive: {
-      color: theme.palette.primary.main,
+      color: theme.palette.text.white
     },
     logout: {
       position: "absolute",
-      bottom: 10,
+      bottom: 0,
       color: theme.palette.secondary.main,
-      backgroundColor: theme.palette.accents.cards,
       fontSize: "14px",
       fontWeight: "400",
       left: 0,
-      right: 0,
-    },
-    plusSign: {
-      position: "relative",
-      top: "5px",
-    },
-    minusSign: {
-      position: "relative",
-      top: "10px",
-    },
-    supportCard: {
-      backgroundColor: theme.palette.accents.cards,
-    },
-  };
-};
+      right: 0
+    }
+  }
+}
